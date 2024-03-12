ff86658d48de230b8c85e88bf03826c75404da8c
Vishvadeep <vishvadeep.0001@gmail.com>
2024-03-12 19:18:34 +0530

    I am going to create portfolio to showcase my skills

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div>
+      <h1 className='text-purple-400'>Google is the search engine in world !</h1>
+    </div>
+  )
+}
+
+export default App
